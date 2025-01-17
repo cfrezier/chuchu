41cb07752422cd6a20d467f149b6cc5d69bfac7f
Christophe Frézier <frezier@gmail.com>
2025-01-17 08:18:48 +0100

    Fixing source maps

---
+++ src/generators/goal/impl/horizontal-line-placement.ts
@@ -0,0 +1,11 @@
+import {Player} from "../../../player";
+import {Goal} from "../../../game/impl/goal";
+import {CONFIG} from "../../../../browser/common/config";
+import {GoalFactory} from "../goal-factory";
+
+export class HorizontalLinePlacement {
+  static implement(players: Player[]): Goal[] {
+    const cellsInSplit = Math.floor(CONFIG.COLUMNS / players.length);
+    return players.map((player, index) => new Goal(GoalFactory.cellNumToPosition([cellsInSplit / 2 + index * cellsInSplit, (CONFIG.ROWS / 2)]), 'up', player));
+  }
+}
